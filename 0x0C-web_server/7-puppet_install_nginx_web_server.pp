02c9187824d91feb800eefcd30c7d1f075f5c035
Khingz <kingsley995mr@gmail.com>
2023-09-27 21:39:52 -0700

    gh

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,6 @@
-# Install and configure nginx
+# Installs a Nginx server
 
-exec {'update package':
-  command =>  '/usr/bin/sudo apt-get update && /usr/bin/sudo apt-get upgrade -y',
-  path    => ['/bin', '/usr/bin']
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-exec {'install nginx':
-  command => '/usr/bin/sudo  apt -y install nginx',
-  path    => ['/bin', '/usr/bin'],
-  require => EXEC['update package']
-}
-
-exec {'manage html page':
-  command => '/usr/bin/sudo mkdir /etc/nginx/html; sudo touch /etc/nginx/html/index.html; sudo chmod +w /etc/nginx/html/index.html',
-  path    => ['/bin', '/usr/bin'],
-  require => EXEC['install nginx']
-}
-
-exec {'write to html file':
-  command =>  '/usr/bin/sudo echo "Hello World!" | sudo tee /etc/nginx/html/index.html > /dev/null',
-  path    => ['/bin', '/usr/bin'],
-  require => EXEC['manage html page']
-}
-
-exec {'configuration':
-  command => '/usr/bin/sudo echo "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /etc/nginx/html;\n\tindex index.html index.htm;\n\tlocation /redirect_me {\n\t\treturn 301 http://khingz.tech/;\n\t}\n\t} | sudo tee /etc/nginx/sites-available/default > /dev/null"',
-  path    => ['/bin', '/usr/bin'],
-  require => EXEC['install nginx']
-}
-
-exec {'restart nginx':
-  command =>  '/usr/bin/sudo nginx -t; /usr/bin/sudo service nginx restart',
-  path    => ['/bin', '/usr/bin'],
-  require => EXEC['configuration']
-}
-
