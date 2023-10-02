32a7eda9c10992614bfd104b3727a2754fadb4d4
Khingz <kingsley995mr@gmail.com>
2023-10-02 12:47:07 -0700

    advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,24 @@
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
+# Installs a Nginx server with custom HTTP header
+
+exec {'package update':
   provider => shell,
+  command  => 'sudo apt-get -y update',
+}
+
+exec {'Add env':
+  provider => shell,
+  command  => 'sudo sed -i "1ienv HOSTNAME;" /etc/nginx/nginx.conf;',
+  require  => Exec['package update'],
+}
+
+exec { 'add header':
+  provider    => shell,
+  command     => 'sudo sed -i "s|index index.html index.htm;|index index.html index.htm;\n\tadd_header X-Served-By \$HOSTNAME;|" /etc/nginx/sites-available/default',
+  require      => Exec['Add env'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  require  => Exec['add header'],
 }
