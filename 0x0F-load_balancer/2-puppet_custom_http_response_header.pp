bc79796c3499e05adc321c3fba00e6364afb1042
Khingz <kingsley995mr@gmail.com>
2023-10-02 08:34:26 -0700

    re implement puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,32 @@
-exec {'install':
+# Installs a Nginx server with custom HTTP header
+
+exec {'package update':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "1ienv HOSTNAME;" /etc/nginx/nginx.conf; sudo sed -i "s/server_name _;/add_header X-Served-By $HOSTNAME;\n\tserver_name _;\n\trewrite ^\/redirect_me https:\/\/https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update',
+}
+
+exec {'Nginx Install':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  require  => Exec['package update'],
+}
+
+exec {'Add env':
+  provider => shell,
+  command  => 'sudo sed -i "1ienv HOSTNAME;" /etc/nginx/nginx.conf;',
+  require  => Exec['Nginx Install'],
+}
+
+exec { 'add header':
+  provider    => shell,
+  command     => 'sudo sed -i "s|index index.html index.htm;|index index.html index.htm;\n\tadd_header X-Served-By \$HOSTNAME;|" /etc/nginx/sites-available/default
+',
+  require      => Exec['Add env'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  require  => Exec['add header'],
 }
 
