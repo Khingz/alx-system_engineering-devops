a8299945f6d60ff157b2c8c8929242e40e8c5d20
Khingz <kingsley995mr@gmail.com>
2023-10-23 10:42:45 -0700

    check failing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,16 +8,14 @@ def main():
     """main api method"""
     url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
     response = requests.get(url)
-    if response.status_code == 200:
-        emp = (response.json())
-        response_2 = requests.get(f'{url}/todos')
-        if response_2.status_code == 200:
-            todo = response_2.json()
-            c = [item for item in todo if item['completed']]
-            print("Employee {} is done with tasks({}/{}):".format(
-                emp.get("name"), len(c), len(todo)))
-            for item in c:
-                print('\t {}'.format(item.get("title")))
+    response_t = response_2 = requests.get(f'{url}/todos')
+    emp = (response.json())
+    todo = response_2.json()
+    c = [item for item in todo if item.get('completed')]
+    print("Employee {} is done with tasks({}/{}):".format(
+        emp.get("name"), len(c), len(todo)))
+    for item in c:
+        print('\t {}'.format(item.get("title")))
 
 
 if __name__ == "__main__":
