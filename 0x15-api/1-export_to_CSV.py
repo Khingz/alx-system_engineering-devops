f44caa12a7ad3251d47b61ef13a2d95950d001ff
Khingz <kingsley995mr@gmail.com>
2023-10-23 14:17:09 -0700

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,19 +4,15 @@ import requests
 from sys import argv
 
 
-def main():
-    """main api method"""
-    url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        emp = (response.json())
-        response_2 = requests.get(f'{url}/todos')
-        if response_2.status_code == 200:
-            todo = response_2.json()
-            c = [item for item in todo if item['completed']]
-            print(f'Employee {emp["name"]} is \
-                    done with tasks({len(c)}/{len(todo)}):
-
-
 if __name__ == "__main__":
-    main()
+    """main api method"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    res = requests.get(url + "users/{}".format(argv[1]))
+    res_t = requests.get(url + "todos", params={"userId": argv[1]})
+    emp = res.json()
+    todo = res_t.json()
+    c = [item for item in todo if item.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        emp.get("name"), len(c), len(todo)))
+    for item in c:
+        print('\t {}'.format(item.get("title")))
